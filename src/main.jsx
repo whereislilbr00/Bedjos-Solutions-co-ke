87f2aa0b4b2eefcd3f69350b0f07a2dc38db716a
whereislilbr00 <louis.lamonte@student.moringaschool.com>
2025-12-23 10:28:01 +0300

    added vercel.json and fixed image paths

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './styles/style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Bedjos-Solutions-co-ke">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
